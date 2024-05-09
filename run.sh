a512aa786f326cbe3270f81c269752074a7f202a
SongYibao <songybmail@foxmail.com>
2024-05-09 21:06:38 +0800

    update: 更新测试编译脚本(仅供开发用)

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@ rm -rf ./build
 # shellcheck disable=SC2164
 mkdir build && cd build && cmake .. && make
 echo "build done,coping plugin ..."
-cp -r ./libplugin-EtherNet-IP.so /home/songyibao/Downloads/neuron-main/build/plugins/
+cp -r ./libplugin-EtherNet-IP.so /software/neuron/build/plugins/
 cp -r ./libplugin-EtherNet-IP.so /opt/neuron/plugins/
+
 cp -r ../EtherNet-IP.json /opt/neuron/plugins/schema/
+cp -r ../EtherNet-IP.json /software/neuron/build/plugins/schema/
 echo "copy done"
